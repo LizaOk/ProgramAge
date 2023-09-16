f509a5f854e7bb62ee00b1034a53fbe0679496d4
LizaOk <fairylizzzok@gmail.com>
2023-09-16 17:23:25 +0300

    Create ProgramAge

---
+++ ProgramAge/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace ProgramAge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelDay = new System.Windows.Forms.Label();
+            this.labelMonth = new System.Windows.Forms.Label();
+            this.labelYear = new System.Windows.Forms.Label();
+            this.buttonReady = new System.Windows.Forms.Button();
+            this.textBoxMonth = new System.Windows.Forms.TextBox();
+            this.textBoxDay = new System.Windows.Forms.TextBox();
+            this.textBoxYear = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxAnswer1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxAnswer4 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxAnswer2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxAnswer3 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(223, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(439, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введіть дату вашого народження";
+            // 
+            // labelDay
+            // 
+            this.labelDay.AutoSize = true;
+            this.labelDay.Font = new System.Drawing.Font("Comic Sans MS", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDay.Location = new System.Drawing.Point(41, 110);
+            this.labelDay.Name = "labelDay";
+            this.labelDay.Size = new System.Drawing.Size(94, 40);
+            this.labelDay.TabIndex = 1;
+            this.labelDay.Text = "День:";
+            this.labelDay.Click += new System.EventHandler(this.labelDay_Click);
+            // 
+            // labelMonth
+            // 
+            this.labelMonth.AutoSize = true;
+            this.labelMonth.Font = new System.Drawing.Font("Comic Sans MS", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMonth.Location = new System.Drawing.Point(303, 110);
+            this.labelMonth.Name = "labelMonth";
+            this.labelMonth.Size = new System.Drawing.Size(121, 40);
+            this.labelMonth.TabIndex = 2;
+            this.labelMonth.Text = "Місяць:";
+            this.labelMonth.Click += new System.EventHandler(this.labelMonth_Click);
+            // 
+            // labelYear
+            // 
+            this.labelYear.AutoSize = true;
+            this.labelYear.Font = new System.Drawing.Font("Comic Sans MS", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelYear.Location = new System.Drawing.Point(579, 110);
+            this.labelYear.Name = "labelYear";
+            this.labelYear.Size = new System.Drawing.Size(63, 40);
+            this.labelYear.TabIndex = 3;
+            this.labelYear.Text = "Рік:";
+            this.labelYear.Click += new System.EventHandler(this.labelYear_Click);
+            // 
+            // buttonReady
+            // 
+            this.buttonReady.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonReady.Location = new System.Drawing.Point(663, 187);
+            this.buttonReady.Name = "buttonReady";
+            this.buttonReady.Size = new System.Drawing.Size(219, 75);
+            this.buttonReady.TabIndex = 4;
+            this.buttonReady.Text = "Готово";
+            this.buttonReady.UseVisualStyleBackColor = true;
+            this.buttonReady.Click += new System.EventHandler(this.buttonReady_Click);
+            // 
+            // textBoxMonth
+            // 
+            this.textBoxMonth.Location = new System.Drawing.Point(443, 110);
+            this.textBoxMonth.Name = "textBoxMonth";
+            this.textBoxMonth.Size = new System.Drawing.Size(100, 40);
+            this.textBoxMonth.TabIndex = 5;
+            this.textBoxMonth.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBoxDay
+            // 
+            this.textBoxDay.Location = new System.Drawing.Point(162, 110);
+            this.textBoxDay.Name = "textBoxDay";
+            this.textBoxDay.Size = new System.Drawing.Size(100, 40);
+            this.textBoxDay.TabIndex = 6;
+            // 
+            // textBoxYear
+            // 
+            this.textBoxYear.Location = new System.Drawing.Point(663, 110);
+            this.textBoxYear.Name = "textBoxYear";
+            this.textBoxYear.Size = new System.Drawing.Size(126, 40);
+            this.textBoxYear.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 34);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Вам";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(180, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 34);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "повних років ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(11, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(163, 34);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Ви прожили";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(281, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 34);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "днів";
+            // 
+            // textBoxAnswer1
+            // 
+            this.textBoxAnswer1.Location = new System.Drawing.Point(74, 66);
+            this.textBoxAnswer1.Multiline = true;
+            this.textBoxAnswer1.Name = "textBoxAnswer1";
+            this.textBoxAnswer1.Size = new System.Drawing.Size(100, 40);
+            this.textBoxAnswer1.TabIndex = 12;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.textBoxAnswer4);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.textBoxAnswer2);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBoxAnswer3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxAnswer1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 268);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(806, 354);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Дані про Вас:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(595, 272);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(65, 34);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "році";
+            // 
+            // textBoxAnswer4
+            // 
+            this.textBoxAnswer4.Location = new System.Drawing.Point(473, 264);
+            this.textBoxAnswer4.Name = "textBoxAnswer4";
+            this.textBoxAnswer4.Size = new System.Drawing.Size(116, 42);
+            this.textBoxAnswer4.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 272);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(461, 34);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Ви досягли (досягнете) повноліття у";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // textBoxAnswer2
+            // 
+            this.textBoxAnswer2.Location = new System.Drawing.Point(175, 130);
+            this.textBoxAnswer2.Name = "textBoxAnswer2";
+            this.textBoxAnswer2.Size = new System.Drawing.Size(100, 42);
+            this.textBoxAnswer2.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(281, 207);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(101, 34);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "місяців";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 207);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(170, 34);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Ви прожили ";
+            // 
+            // textBoxAnswer3
+            // 
+            this.textBoxAnswer3.Location = new System.Drawing.Point(175, 199);
+            this.textBoxAnswer3.Name = "textBoxAnswer3";
+            this.textBoxAnswer3.Size = new System.Drawing.Size(100, 42);
+            this.textBoxAnswer3.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 654);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBoxYear);
+            this.Controls.Add(this.textBoxDay);
+            this.Controls.Add(this.textBoxMonth);
+            this.Controls.Add(this.buttonReady);
+            this.Controls.Add(this.labelYear);
+            this.Controls.Add(this.labelMonth);
+            this.Controls.Add(this.labelDay);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Program";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelDay;
+        private System.Windows.Forms.Label labelMonth;
+        private System.Windows.Forms.Label labelYear;
+        private System.Windows.Forms.Button buttonReady;
+        private System.Windows.Forms.TextBox textBoxMonth;
+        private System.Windows.Forms.TextBox textBoxDay;
+        private System.Windows.Forms.TextBox textBoxYear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxAnswer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxAnswer4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxAnswer2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxAnswer3;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ ProgramAge/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Windows.Forms;
+ 
+
+namespace ProgramAge
+{
+    public partial class Form1 : Form
+    {
+        
+        public void CalculateYears ()
+        {
+            string text = textBoxYear.Text;  
+            int year = Convert.ToInt32(text);
+
+            int answer1 = DateTime.Now.Year - year;
+
+            textBoxAnswer1.Text += answer1;
+        }
+
+        public void CalculateMonths()
+        {
+            int year = Convert.ToInt32(textBoxYear.Text);
+            int day = Convert.ToInt32(textBoxDay.Text);
+            int month = Convert.ToInt32(textBoxMonth.Text);
+
+            DateTime date1 = new DateTime(year, month, day);
+            DateTime date2 = new DateTime(2023, 9, 16);
+
+            TimeSpan difference = date2 - date1;
+            int daysDifference = difference.Days;
+
+            textBoxAnswer2.Text += daysDifference;
+        }
+
+            public void CalculateDays()
+        {
+            int year = Convert.ToInt32(textBoxYear.Text);
+            int day = Convert.ToInt32(textBoxDay.Text);
+            int month = Convert.ToInt32(textBoxMonth.Text);
+
+            DateTime date1 = new DateTime(year, month, day);
+            DateTime date2 = new DateTime(2023, 9, 16);
+
+            int monthsDifference = ((date2.Year - date1.Year) * 12) + date2.Month - date1.Month;
+
+            textBoxAnswer3.Text += monthsDifference;
+        }
+        public void CalculateComingOfAge()
+        {
+            string text = textBoxYear.Text;
+            int year = Convert.ToInt32(text);
+
+            int answer4 = year + 18;
+
+            textBoxAnswer4.Text += answer4;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonReady_Click(object sender, EventArgs e)
+        {
+            CalculateYears();
+            CalculateDays();
+            CalculateMonths();
+            CalculateComingOfAge();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelYear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelMonth_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelDay_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
